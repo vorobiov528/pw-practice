88562e9db0eb5ec0eaf424a318c82752635b7cf9
sdet <vorobiov528m@gmail.com>
2024-06-19 13:01:42 -0700

    User-Visible-locator

---
+++ tests/userVisibleLacators.spec.ts
@@ -0,0 +1,30 @@
+import { test } from '@playwright/test';
+
+test.beforeEach(async({page})=>{
+  await page.goto('http://localhost:4200/');
+  await page.getByText('Forms').click();
+  await page.getByText('Form Layouts').click();
+
+})
+
+test('User-Facing-Locator',async ({page})=>{
+
+ await page.getByRole('textbox',{name: "Email"}).first().click();
+ await page.getByRole('button',{name: "Sign in"}).first().click();
+
+ await page.getByLabel('Email').first().click();
+
+ await page.getByPlaceholder('Jane Doe').first().click();
+
+ await page.getByText('Using the Grid').first().click();
+
+ await page.getByTitle('Using the Grid').first().click();
+
+
+ await page.getByTestId('Sign').first().click();
+
+
+
+
+
+})

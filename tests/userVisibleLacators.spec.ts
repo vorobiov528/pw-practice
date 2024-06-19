8f42e5b8a0fc5800c7a3dfadf17e165402d4a65d
sdet <vorobiov528m@gmail.com>
2024-06-19 13:33:01 -0700

    improved/added comments

--- tests/userVisibleLacators.spec.ts
+++ tests/userVisibleLacators.spec.ts
@@ -6,6 +6,11 @@ test.beforeEach(async({page})=>{
   await page.getByText('Form Layouts').click();
 
 })
+// input field- page.getByRole('textbox')
+// page.getByRole('button',{name:'Sign In'})
+//lable - page.getByLable('Email)
+//placeholder - page.getByPlaceHolder('Email)
+// text - page.getBytext('SignIn')
 
 test('User-Facing-Locator',async ({page})=>{
 

1bdaea1d61db597a62b2bf3f6b5e55a12b04174d
sdet <vorobiov528m@gmail.com>
2024-06-22 13:15:42 -0700

    reusible-locator cose added

--- tests/userVisibleLacators.spec.ts
+++ tests/userVisibleLacators.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test.beforeEach(async({page})=>{
   await page.goto('http://localhost:4200/');
@@ -39,7 +39,16 @@ test('Parent-Element',async ({page})=>{
   await page.locator('nb-card').filter({hasText:"Basic Form"}).getByRole('textbox',{name: "Email"}).click();
   await page.locator('nb-card').filter({has:page.locator("Basic Form")}).getByRole('textbox',{name: "Email"}).click();
   await page.locator('nb-card').filter({has:page.locator("check-box")}).filter({hasText:"Sign in"}).getByRole('textbox',{name: "Email"}).click();
+// Reusible locators
+test('Reusible-Locator',async ({page})=>{   
+    const basicForm = page.locator('nb-card').filter({hasText:'Basic form'});
+    const emailField = basicForm.getByRole('textbox',{name: "Email"});
 
+    await emailField.fill('test@test.co');
+    await basicForm.getByRole('textbox',{name: "Password"}).fill('Welcome123');
+    await basicForm.locator('nb-checkbox').click();
+    await basicForm.getByRole('button').click()
+
+  await expect(emailField).toHaveValue('test@test.com')
+  })
 })
-
-

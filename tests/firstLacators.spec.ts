86296387697a12426affb95ce963dd2e49310481
sdet <vorobiov528m@gmail.com>
2024-06-26 20:35:36 -0700

    to see the code for practice

--- tests/firstLacators.spec.ts
+++ tests/firstLacators.spec.ts
@@ -6,11 +6,41 @@ test.beforeEach(async({page})=>{
   await page.getByText('Form Layouts').click();
 
 })
+
+test('Locator-Rules',async ({page})=>{
+  // by Tag Name 
+   page.locator('input');
+ 
+   //by id
+   page.locator('#inputEmail1');
+ 
+   //by class-value
+   page.locator('.shape-rectangle');
+   //by full class
+   page.locator('class="input-full-width size-medium status-basic shape-rectangle nb-transition"')
+
+  // by attribute
+  //await page.locator('placeholder="Email"').click();
+  //await page.locator('placeholder="Email"').first().click();
+  page.locator('placeholder="Email"')
+  //DIFFERENT SELECTORS
+  page.locator('input[placeholder="Email"][nbinput]');
+  //or
+  page.locator('input[placeholder="Email1"].[size-medium]'); 
+
+  //by partial TExt
+  page.locator(':text("Using")');
+ 
+   //by Exact Match TExt
+   page.locator(':text("Using the Grid")');  
+})
 // input field- page.getByRole('textbox')
 // page.getByRole('button',{name:'Sign In'})
 //lable - page.getByLable('Email)
 //placeholder - page.getByPlaceHolder('Email)
 // text - page.getBytext('SignIn')
+
+
 
 test('User-Facing-Locator',async ({page})=>{
   await page.getByRole('textbox',{name: "Email"}).first().click();
@@ -39,7 +69,10 @@ test('Parent-Element',async ({page})=>{
   await page.locator('nb-card').filter({hasText:"Basic Form"}).getByRole('textbox',{name: "Email"}).click();
   await page.locator('nb-card').filter({has:page.locator("Basic Form")}).getByRole('textbox',{name: "Email"}).click();
   await page.locator('nb-card').filter({has:page.locator("check-box")}).filter({hasText:"Sign in"}).getByRole('textbox',{name: "Email"}).click();
-// Reusible locators
+
+
+
+  // Reusible locators
 test('Reusible-Locator',async ({page})=>{   
     const basicForm = page.locator('nb-card').filter({hasText:'Basic form'});
     const emailField = basicForm.getByRole('textbox',{name: "Email"});
@@ -89,7 +122,7 @@ test('Assertion',async({page})=>{
     //Soft Assertions continue execution after faild
     await expect.soft(basicFormButton).toHaveText('Submit')
 })
-
+test('AUTO-WAITING',async({page})=>{
 // for autowaiting we can use: 
 // await expect(seectorSuccsesBtn).toHaveText('text',{timeout:20000})
 //or wait for network call be completed 
@@ -97,4 +130,7 @@ test('Assertion',async({page})=>{
 // or WAIT FORE ALL API CALL COMPLETED -> await page.waitForLoadState('networkidle')
 // or
 //page.waitForTimeout
+
+})
+
 })
